b546f8d4cf2cf76f899646aed27964f23b25b17f
Brendon Mackenzie <brendon@primitiveman.co.nz>
2017-07-16 20:06:23 +1200

    added basic version of make_snake_case

--- utils.py
+++ utils.py
@@ -3,5 +3,17 @@ Some useful utilities for python, such as current directory, etc.
 """
 
 
+def make_snake_case(text: str) -> str:
+    """
+    A very basic way to converts some text into snake case.
+    Strips out any non a-z, 0-9 characters.
+    :param text:
+    :return: a string which snake cases the text provided
+    """
+    chars = 'abcdefghijklmnopqrstuvwxyz1234567890_'
+    unclean = text.lower().replace(' ', '_')
+    return ''.join(e for e in unclean if e in chars)
+
+
 if __name__ == '__main__':
-    pass
+    print(make_snake_case('Héllø Wörld'))
